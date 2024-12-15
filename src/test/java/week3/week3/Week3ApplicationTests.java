0b8ace31e0edbd2c0bcbe49fe91a8544d4f71ec0
Aman Shekhar <amanshekhar66@gmail.com>
2024-12-15 13:27:45 +0530

    First Commit

---
+++ src/test/java/week3/week3/Week3ApplicationTests.java
@@ -0,0 +1,13 @@
+package week3.week3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
